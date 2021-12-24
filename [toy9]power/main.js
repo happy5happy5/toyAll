1923216b260d2c89cb7d5d9bfb0163ba59170ff4
happy5happy5 <zombil8731@gmail.com>
2021-12-24 11:34:30 +0900

    fix

--- [toy9]power/main.js
+++ [toy9]power/main.js
@@ -1,21 +1,21 @@
 
-
 function power(base, exponent) {
   // todo: 여기에 코드를 작성합니다.
-    let sth=1000;
-    if (exponent===1)return base%sth
-    let half = parseInt(exponent/2)
-    let temp=power(base,half)
-    
-    if (exponent%2===1)return temp*temp*base%sth
-    else return temp*temp%sth
-
+  let sth=94906249
+  if(exponent===0)return 1
+  let half=parseInt(exponent/2)
+  let temp=power(base, half)
+  let result = (temp * temp) % sth
+  if(exponent%2===1)return result*base%sth
+  else return result % sth
 }
 
   base=4
   exponent=20
+  // 29773085
   //94906249
- console.log( power(base, exponent))
+  // debugger
+  console.log( power(base, exponent))
 
  //result=                                 4**20%1000 이게 목표
 //result=                     (4**10%1000)                    *               (4**10%1000)%1000
