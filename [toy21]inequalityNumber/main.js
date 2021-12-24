93791ef1931627b4d7838ab6cc8fd4e7afddff0a
citysquirrel <tok1324@naver.com>
2021-12-24 17:02:04 +0900

    ref랑 차이 있는 origin 풀이만 일단 업데이트

--- [toy21]inequalityNumber/main.js
+++ [toy21]inequalityNumber/main.js
@@ -1,56 +1,88 @@
-let sign = '< > > <'
-sign = sign.split(' ')
-result=+maxList(sign)- minList(sign)
-console.log(result)
+let sign = "< > > <";
+sign = sign.split(" ");
+result = +maxList(sign) - minList(sign);
+console.log(result);
 
-function minList(sign){
-    let queMin = []
-    for (let i = 9; i >= 9 - sign.length; i--) {
-        queMin.push(9 - i)
+function minList(sign) {
+  let queMin = [];
+  for (let i = 9; i >= 9 - sign.length; i--) {
+    queMin.push(9 - i);
+  }
+  let queNum = []; //완성품
+  let waiting = []; //대기줄
+
+  waiting.push(queMin[0]);
+  for (let i = 0, j = 0, m = 0; i < sign.length; i++) {
+    if (sign[i] === "<") {
+      queNum.push(...waiting);
+      ++m;
+      j = m;
+      waiting = [];
+      waiting.push(queMin[j]);
+    } else if (sign[i] === ">") {
+      waiting = waiting.map((x) => x + 1);
+      ++m;
+      waiting.push(queMin[j]);
     }
-    let queNum = [] //완성품
-    let waiting = [] //대기줄
-    
-    waiting.push(queMin[0])
-    for (let i = 0, j = 0, m = 0; i < sign.length; i++) {
-        if (sign[i] === '<') {
-            queNum.push(...waiting)
-            ++m
-            j = m
-            waiting = []
-            waiting.push(queMin[j])
-    
-        } else if (sign[i] === '>') {
-            waiting = waiting.map(x => x + 1)
-                ++m
-            waiting.push(queMin[j])
-        }
-    }
-    queNum.push(...waiting)
-    return queNum.join('')
+  }
+  queNum.push(...waiting);
+  return queNum.join("");
 }
 
 function maxList(sign) {
-    let queMax = []
-    for (let i = 9; i >= 9 - sign.length; i--) {
-        queMax.push(i)
+  let queMax = [];
+  for (let i = 9; i >= 9 - sign.length; i--) {
+    queMax.push(i);
+  }
+  let queNum = []; //완성품
+  let waiting = []; //대기줄
+  waiting.push(queMax[0]);
+  for (let i = 0, j = 0, m = 0; i < sign.length; i++) {
+    if (sign[i] === "<") {
+      waiting = waiting.map((x) => x - 1);
+      ++m;
+      waiting.push(queMax[j]);
+    } else if (sign[i] === ">") {
+      queNum.push(...waiting);
+      ++m;
+      j = m;
+      waiting = [];
+      waiting.push(queMax[j]);
     }
-    let queNum = [] //완성품
-    let waiting = [] //대기줄
-    waiting.push(queMax[0])
-    for (let i = 0, j = 0, m = 0; i < sign.length; i++) {
-        if (sign[i] === '<') {
-            waiting = waiting.map(x => x - 1)
-                ++m
-            waiting.push(queMax[j])
-        } else if (sign[i] === '>') {
-            queNum.push(...waiting)
-                ++m
-            j = m
-            waiting = []
-            waiting.push(queMax[j])
-        }
-    }
-    queNum.push(...waiting)
-    return queNum.join('')
+  }
+  queNum.push(...waiting);
+  return queNum.join("");
 }
+
+// 도시다람쥐
+const inequalityNumberbySquirrel = function (signs) {
+  // edge case: 연속해서 같은 부등호가 들어오는 경우
+  // 애초부터 배치를 해놓고 하나씩 정렬하는 방법은 어떨까?
+  // bubble sort 느낌으로..
+  const list = signs.split(" ");
+  const fromNine = Array.from(
+    { length: list.length + 1 },
+    (undefined, x) => 9 - x
+  );
+  const fromZero = Array.from({ length: list.length + 1 }, (undefined, x) => x);
+
+  const inequalitySort = (list, nums) => {
+    let isOn = true;
+    while (isOn) {
+      isOn = false;
+      for (let i = 0; i < list.length; i++) {
+        if (
+          (list[i] === ">" && nums[i] < nums[i + 1]) ||
+          (list[i] === "<" && nums[i] > nums[i + 1])
+        ) {
+          [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+          isOn = true;
+        }
+      }
+    }
+    return nums.join("");
+  };
+  const max = inequalitySort(list, fromNine);
+  const min = inequalitySort(list, fromZero);
+  return max - min;
+};
--- [toy29]binaryHeap/main.js
+++ [toy29]binaryHeap/main.js
@@ -1,28 +1,43 @@
-arr=[4, 10, 3, 5, 1]
+arr = [4, 10, 3, 5, 1];
 function swap(idx1, idx2, arr) {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 }
-function binaryHeap (arr) {
-    return arr.reduce((heap, item) => {
-        return insert(heap, item);
-    }, []);
+function binaryHeap(arr) {
+  return arr.reduce((heap, item) => {
+    return insert(heap, item);
+  }, []);
 }
 function getParentIdx(idx) {
-    if(idx===0)return 0;
-    return parseInt((idx-1)/2)
+  if (idx === 0) return 0;
+  return parseInt((idx - 1) / 2);
 }
 
 function insert(heap, item) {
-    let son=heap.length
-    let god=getParentIdx(son)
-    heap.push(item)
-    while(heap[god]<heap[son]){
-        swap(god, son, heap)
-        if(god!==0)[god,son]=[getParentIdx(god),god]
-    }
-    return heap
+  let son = heap.length;
+  let god = getParentIdx(son);
+  heap.push(item);
+  while (heap[god] < heap[son]) {
+    swap(god, son, heap);
+    if (god !== 0) [god, son] = [getParentIdx(god), god];
+  }
+  return heap;
 }
 
+// 도시다람쥐: 재귀형태 저장용
+function insertUsingRecursionBySq(heap, item) {
+  heap.push(item);
+  let childIdx = heap.length - 1;
+  let parentIdx = getParentIdx(childIdx);
+
+  const swapRec = (parent, child) => {
+    if (heap[parent] < heap[child]) {
+      swap(parent, child, heap);
+      swapRec(getParentIdx(parent), parent);
+    }
+    if (parent === 0) return;
+  };
+  swapRec(parentIdx, childIdx);
+}
 
 // debugger
-console.log(binaryHeap (arr))
+console.log(binaryHeap(arr));
