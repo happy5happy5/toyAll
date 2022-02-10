60ab9ef77cc0ee317f02a392d659c9617ab1bc19
happy5happy5 <zombil8731@gmail.com>
2022-02-10 14:08:52 +0900

    Update main.js

--- [toy36]closestPairOfPoints/main.js
+++ [toy36]closestPairOfPoints/main.js
@@ -56,7 +56,7 @@ console.log(Math.round(closestPair(points)*100))
 
 // coplit pass code
 
-// // 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
+// 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
 // function calculateDistance(p1, p2) {
 //   const yDiff = p2[0] - p1[0];
 //   const xDiff = p2[1] - p1[1];
