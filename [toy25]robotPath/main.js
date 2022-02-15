a1ac3f1f186457783235232cf71a4ce762d4d0dc
happy5happy5 <zombil8731@gmail.com>
2022-02-15 20:17:34 +0900

    Update main.js

--- [toy25]robotPath/main.js
+++ [toy25]robotPath/main.js
@@ -1,29 +1,20 @@
-let room = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0],
-    [1, 0, 0, 0, 0, 0],
-  ];
-  let sta = [4, 2];
-  let end = [2, 2];
-  let que={}
-  let level=0;
+//happy5happy5
+const robotPath = function (room, src, dst) {
+
   function indicator(room,sta){//열려있는 길의 좌표 배열을 반환
       let path=[]
+      let [i,j]=sta
       let [height,width]=[room.length-1,room[0].length-1]
-    if(room[sta[0]][sta[1]]===0){
-        if(sta[1]+1<=width&&room[sta[0]][sta[1]+1]===0)path.push([sta[0],sta[1]+1])
-        if(sta[0]+1<=height&&room[sta[0]+1][sta[1]]===0)path.push([sta[0]+1,sta[1]])
-        if(sta[1]-1>=0&&room[sta[0]][sta[1]-1]===0)path.push([sta[0],sta[1]-1])
-        if(sta[0]-1>=0&&room[sta[0]-1][sta[1]]===0)path.push([sta[0]-1,sta[1]])
+    if(room[i][j]===0){
+        if(j+1<=width&&room[i][j+1]===0)path.push([i,j+1])
+        if(i+1<=height&&room[i+1][j]===0)path.push([i+1,j])
+        if(j-1>=0&&room[i][j-1]===0)path.push([i,j-1])
+        if(i-1>=0&&room[i-1][j]===0)path.push([i-1,j])
     }
     return path
   }
-
-  function pathFinder(room,sta,end,que,level){
+  function pathFinder(room,sta,end,que={},level=0){
     que[sta]=level;
-    console.log(sta)
     if(`${end}`===`${sta}`)return que[sta]
 
     let path=indicator(room,sta)
@@ -34,8 +25,6 @@ let room = [
         pathFinder(room,next,end,que,level+1)
     }
     return que
-    
-
   }
-  pathFinder(room,sta,end,que,level)
-  console.log(que[end])
+  return pathFinder(room,src,dst)[dst]
+};
