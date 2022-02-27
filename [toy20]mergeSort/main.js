0b1caaa30d67ee459b17ace57a053c2a789af719
happy5happy5 <zombil8731@gmail.com>
2022-02-27 13:14:19 +0900

    Update pretty short code

--- [toy20]mergeSort/main.js
+++ [toy20]mergeSort/main.js
@@ -1,21 +1,14 @@
 //happy5happy5
-//mergeSort last
-Array.prototype.mergeSort = function(cal=(a,b)=>a-b){
-  let arr=this;
-  function downtier(arr){
-      if(arr.length<=1)return arr
-      let half=parseInt(arr.length/2)
-      return uptier(downtier(arr.slice(0,half)),downtier(arr.slice(half)))
-  }
-  function uptier(left,right,arr=[]){
-      while(left.length&&right.length){
-      if(cal(left[0],right[0])<0)arr.push(left.shift())
-      else if(cal(left[0],right[0])>0)arr.push(right.shift())
-      else{
-          arr.push(left.shift())
-          arr.push(right.shift())
-      }}
-      return [...arr,...left,...right]
-  }
-  return downtier(arr)
+Array.prototype.mergeSort = function(cal=(a,b)=>a-b,arr=this){
+    const downtier=(arr)=>arr.length<=1?arr:uptier(downtier(arr.slice(0,parseInt(arr.length/2))),downtier(arr.slice(parseInt(arr.length/2))));
+    const uptier=(left,right,arr=[],leftIdx=0,rightIdx=0)=>{
+        while (leftIdx+rightIdx<left.length+right.length)
+            if (leftIdx >= left.length)arr.push(right[rightIdx++]);
+            else if (rightIdx >= right.length||cal(left[leftIdx],right[rightIdx])<0)arr.push(left[leftIdx++]);
+            else arr.push(right[rightIdx++]);
+        return arr;
+    }
+    return downtier(arr);
 }
+//arr.mergeSort()의 형태로 사용
+//arr.mergeSort((a,b)=>b-a)는 내림차순
