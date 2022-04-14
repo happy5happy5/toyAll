3bc8e9d1f9d770499cbe37b166d6ca0722272edd
happy5happy5 <zombil8731@gmail.com>
2022-04-14 19:26:20 +0900

    Update main.js

--- programmers/무지의 먹방 라이브/main.js
+++ programmers/무지의 먹방 라이브/main.js
@@ -2,6 +2,7 @@
 // [3, 1, 2]	5	1
 //[2,0,1] 2
 //[1,0,0] 0
+//0. 정전되기전에 모든 음식을 먹는지 확인하고 먹을수있으면 -1 반환
 //1. 인덱스를 포함하는 새로운 객체 만든다.
 //2. 벨류로 솔트한다.
 //3. k가 음수가 안된다면 객체 크기만큼 k에서 빼준다
@@ -11,38 +12,39 @@
 //7. 지금부터는 테이블을 k가 0이 될때까지 돌려도 어떤 음식도 0이 되지않는다.
 //8. k% m.length 에 -1을 한 객체값의 인덱스를 반환하면 정답.
 function solution(food_times, k) {
+  //1
   let m = food_times.map((food, idx) => {
-    //1
     return { food, idx: idx + 1 };
   });
   let sum = m.reduce((a, b) => {
     return a + b.food;
   }, 0);
-  if (k >= sum) return -1;
+  if (k >= sum) return -1; //0
 
   m.sort((a, b) => a.food - b.food); //2
-  // let minusSum = 0;
   let len = m.length;
-  // let acc=0;
-  // let door = true;
+  let acc = 0;
+  let door = true;
   for (let i = 0; i < m.length; i++) {
     let temp = m[i].food;
-    if (k - temp * len >= 0) {
-      if (temp) {
-        k = k - temp * len;
-        m = m.map((x) => {
-          if (x.food - temp === 0) len--;
-          if (x.food === 0) return x;
-          return { food: x.food - temp, idx: x.idx };
-        });
-      }
+    if (door && temp === acc) {
+      k -= acc;
+      m[i].food = 0;
+      len--;
+      continue;
+    }
+    if (door && k - temp * len >= 0) {
+      acc += temp - acc;
+      m[i].food = 0;
+      len--;
+      k -= acc;
     } else {
-      break;
+      if (door) door = false;
+      m[i].food -= acc;
+      k -= acc;
     }
   }
   m = m.filter((x) => !!x.food).sort((a, b) => a.idx - b.idx);
-  // console.log(m);
-  // console.log(k);
 
   return m[k % m.length].idx;
 }
@@ -51,9 +53,13 @@ let n = solution([52, 62, 22, 2, 2, 2, 102], 100);
 
 console.log(n);
 // [52, 62, 22, 2, 2, 2, 102], 100
-// [50, 60, 20, 0, 0, 0, 100], 86
-// [30, 40, 0, 0, 0, 0, 80], 6
+// [2, 2, 2, 22, 52, 62, 102], 100 acc=0
 
-// [4, 5, 1, 0, 0, 0, 9], 2
-// [4, 5, 1, 0, 0, 0, 9], 2
-// [3, 4, 1, 0, 0, 0, 9], 0
+// [0, 2, 2, 22, 52, 62, 102], 98 acc=2
+// [0, 0, 2, 22, 52, 62, 102], 96 acc=2
+// [0, 0, 0, 22, 52, 62, 102], 94 acc=2
+// [0, 0, 0, 0, 52, 62, 102], 72 acc=22
+
+// [0, 0, 0, 0, 30, 62, 102], 50 acc=22
+// [0, 0, 0, 0, 30, 40, 102], 28 acc=22
+// [0, 0, 0, 0, 30, 40, 80], 6 acc=22
