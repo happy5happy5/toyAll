a5684cc026cee3e42f7dc7276c643d1fbb2083b9
happy5happy5 <zombil8731@gmail.com>
2022-04-14 14:03:32 +0900

    Update main.js

--- programmers/호텔 방 배정/main.js
+++ programmers/호텔 방 배정/main.js
@@ -26,22 +26,22 @@ console.log(solution(k, room_number));
 
 //객체 안통함
 
-function solution(k, room_number) {
-  let hotel = {};
-  let answer = [];
-  const spliter = (num) => {
-    if (!hotel[num]) {
-      hotel[num] = num + 1;
-      return num;
-    }
-    let temp = spliter(hotel[num]);
-    hotel[num] = temp + 1;
-    return temp;
-  };
-  for (let i = 0; i < room_number.length && i < k; i++) {
-    answer.push(spliter(room_number[i]));
-  }
+// function solution(k, room_number) {
+//   let hotel = {};
+//   let answer = [];
+//   const spliter = (num) => {
+//     if (!hotel[num]) {
+//       hotel[num] = num + 1;
+//       return num;
+//     }
+//     let temp = spliter(hotel[num]);
+//     hotel[num] = temp + 1;
+//     return temp;
+//   };
+//   for (let i = 0; i < room_number.length && i < k; i++) {
+//     answer.push(spliter(room_number[i]));
+//   }
 
-  // console.log(hotel);
-  return answer;
-}
+//   // console.log(hotel);
+//   return answer;
+// }
