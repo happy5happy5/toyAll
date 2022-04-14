e5cbe4fa6743f7b9a17209c64f905c741f9eade8
happy5happy5 <zombil8731@gmail.com>
2022-04-14 14:03:34 +0900

    Update main.js

--- programmers/부족한 금액 계산하기/main.js
+++ programmers/부족한 금액 계산하기/main.js
@@ -1,10 +1,10 @@
 function solution(price, money, count) {
-    var answer = 0;
-    let temp=money;
-    for(let i=0;i<count;i++){
-        temp-=price*(i+1)
-        if(temp<0)answer=-temp
-    }
+  var answer = 0;
+  let temp = money;
+  for (let i = 0; i < count; i++) {
+    temp -= price * (i + 1);
+    if (temp < 0) answer = -temp;
+  }
 
-    return answer;
+  return answer;
 }
